d79ce2e1260d1d014bb6a57434ac364f2a965fb9
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-08-09 17:08:07 +0000

    no bookmark updates

--- lib/components/no_bookmark.dart
+++ lib/components/no_bookmark.dart
@@ -1,4 +1,6 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class NoBookmark extends StatefulWidget {
   const NoBookmark({super.key});
@@ -8,8 +10,65 @@ class NoBookmark extends StatefulWidget {
 }
 
 class _NoBookmarkState extends State<NoBookmark> {
+  final List<String> gifData = [
+    'assets/images/travel.gif',
+    'assets/images/travel2.gif',
+    'assets/images/travel3.gif',
+    'assets/images/travel4.gif',
+  ];
+
+  late String selectedGif;
+
+  @override
+  void initState() {
+    super.initState();
+    // Select a random GIF when the widget is initialized
+    final random = Random();
+    selectedGif = gifData[random.nextInt(gifData.length)];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      padding: const EdgeInsets.all(20.0),
+      height: 320.0,
+      width: 320.0,
+      decoration: BoxDecoration(
+        color: const Color(0xFF494B45).withOpacity(0.4),
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'No planned travels yet...',
+            style: GoogleFonts.inter(
+              color: const Color(0xFFFFFFFF),
+              fontWeight: FontWeight.w500,
+              fontSize: 18.0,
+            ),
+          ),
+          Text(
+            textAlign: TextAlign.center,
+            'Plan your travels with this AI enhanced application',
+            style: GoogleFonts.inter(
+              color: const Color(0xFFFFFFFF),
+              fontWeight: FontWeight.w300,
+              fontSize: 14.0,
+            ),
+          ),
+
+          // Display the selected GIF here
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: Image.asset(
+              selectedGif,
+              height: 180.0,
+              width: 180.0,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
