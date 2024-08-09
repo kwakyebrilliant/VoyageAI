43f52a1bde4cadd0e799343407d8f1bb4edbba8a
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-08-09 17:03:48 +0000

    no bookmark added

---
+++ lib/components/no_bookmark.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NoBookmark extends StatefulWidget {
+  const NoBookmark({super.key});
+
+  @override
+  State<NoBookmark> createState() => _NoBookmarkState();
+}
+
+class _NoBookmarkState extends State<NoBookmark> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
