b6f37ce6b7e91401b6c3787db635a6aafbba2e1a
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-08-14 21:01:06 +0000

    details updates

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DetailScreen extends StatelessWidget {
   final String bookmark;
@@ -13,36 +14,71 @@ class DetailScreen extends StatelessWidget {
           SliverAppBar(
             backgroundColor: const Color(0xFFFFFFFF),
             flexibleSpace: FlexibleSpaceBar(
+              background: Image.asset(
+                "assets/images/map.png",
+                fit: BoxFit.cover,
+              ),
               centerTitle: true,
-              title: IconButton(
-                icon: Container(
-                  height: 40.0,
-                  width: 40.0,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: const Color(0xFFF6C00A),
-                    border: Border.all(
-                      color: const Color(0xFF494B45).withOpacity(0.2),
-                      width: 0.2,
-                    ),
-                  ),
-                  child: const Icon(
-                    Icons.close_outlined,
-                    color: Color(0xFFFFFFFF),
-                    size: 40.0,
-                  ),
+              title: Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 6.0,
+                  horizontal: 10.0,
                 ),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
+                width: 125.0,
+                decoration: BoxDecoration(
+                  color: const Color(0xFF494B45).withOpacity(0.7),
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      Icons.location_on_rounded,
+                      color: Color(0xFFF6C00A),
+                      size: 20.0,
+                    ),
+                    Text(
+                      'Paris, France',
+                      style: GoogleFonts.inter(
+                        color: const Color(0xFFFFFFFF),
+                        fontWeight: FontWeight.w300,
+                        fontSize: 14.0,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
               ),
             ),
             floating: true,
             snap: true,
-            expandedHeight: 80.0,
+            expandedHeight: 120.0,
             collapsedHeight: 52.0,
             toolbarHeight: 52.0,
             pinned: true,
+            automaticallyImplyLeading: false,
+            leading: IconButton(
+              icon: Container(
+                height: 30.0,
+                width: 30.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: const Color(0xFFFF0000),
+                  border: Border.all(
+                    color: const Color(0xFF494B45).withOpacity(0.2),
+                    width: 0.2,
+                  ),
+                ),
+                child: const Icon(
+                  Icons.close_rounded,
+                  color: Color(0xFFFFFFFF),
+                  size: 30.0,
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
           ),
           SliverToBoxAdapter(
             child: Padding(
